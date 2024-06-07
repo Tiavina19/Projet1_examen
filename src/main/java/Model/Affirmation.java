b3ed3f72e80479afddbd7378d0f77e259d3d0b45
Tiavina19 <hei.tiavina@gmail.com>
2024-06-07 16:17:33 +0300

    feat: add affirmationLogique

--- src/main/java/Model/Affirmation.java
+++ src/main/java/Model/Affirmation.java
@@ -1,17 +1,3 @@
 package Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-public class Affirmation {
-    private boolean estBeau;
-    private boolean estPauvre;
-    private boolean estGenereux;
-}
 
--- src/main/java/affirmationLogique/DoncLogique.java
+++ src/main/java/affirmationLogique/DoncLogique.java
@@ -7,12 +7,12 @@ public class DoncLogique {
         return !affirmation.isEstBeau() ||affirmation.isEstPauvre();
     }
 
-    // Méthode statique pour vérifier si Lou est pauvre donc Lou est généreux
+
     public static boolean pauvreDoncGenereux(Affirmation affirmation) {
         return !affirmation.isEstPauvre() || affirmation.isEstGenereux();
     }
 
-    // Méthode statique pour vérifier si Lou est beau ou généreux donc Lou est pauvre
+
     public static boolean beauOuGenereuxDoncPauvre(Affirmation affirmation) {
         return !(affirmation.isEstBeau() || affirmation.isEstGenereux()) || affirmation.isEstPauvre();
     }
