88cba1b9dbbe63c88cac30e44f12f6e7a979f298
Tiavina19 <hei.tiavina@gmail.com>
2024-06-07 14:24:58 +0300

    first commit

---
+++ src/main/java/com/example/calculusrationator/CalculusRationatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculusrationator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculusRationatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculusRationatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculusrationator/CalculusRationatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculusrationator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculusRationatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
